6159474a4e52e127ff1ee75cfc0ca80e69d8290b
Bastien <bastien.meziere@protonmail.com>
2021-05-28 22:13:19 +0200

    Modification chemin IMAGE & SCRIPT

--- define.php
+++ define.php
@@ -1,8 +1,8 @@
 <?php 
 define('WEBROOT',str_replace('index.php','',$_SERVER['SCRIPT_NAME']));
 define('ROOT',str_replace('index.php','',$_SERVER['SCRIPT_FILENAME']));
-define('IMAGE','http://localhost/desbarreslessio3/asset/');
-define('SCRIPT','http://localhost/desbarreslessio3/asset/js/');
+define('IMAGE','http://localhost/DesBarresLesSio/asset/');
+define('SCRIPT','http://localhost/DesBarresLesSio/asset/js/');
 
 
 
